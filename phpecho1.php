237368f274eb0afb37f2436453eb6ebddcd32b93
sandeepkonakanchi <sxk80270@ucmo.edu>
2017-04-07 17:13:47 -0400

    this is a sample php file

---
+++ phpecho1.php
@@ -0,0 +1,25 @@
+<?php
+
+    //echo "<h1> This is PHP data type example</h1>";
+    echo "hello everyone</br>";
+    echo "<h2>I'm learning PHP &nbsp";
+    echo "at W3schools.com</h2>";
+    //printing string with multiple parameters,....
+    echo "this ","is a ", "string ", "with ", "multiple ", "parameters";
+    
+    $txt1 = "learning PHP";
+    $txt2 = "W3 schools.com";
+     $a=5;
+     $b=9;
+    
+    echo "<h1>$txt1</h1>";
+    echo "$txt1 at $txt2 </br>";
+    echo $a+$b;
+   
+    print "$txt1 at $txt2 </br>";
+    print $a+$b;
+    echo "this ","is a ", "string ", "with ", "multiple ", "parameters";
+
+    ?>
+    
+    
