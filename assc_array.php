acfdbf1ddd4ed6418f3f2dc58fc5b72a195bdf2e
sandeepkonakanchi <sxk80270@ucmo.edu>
2018-01-27 00:55:50 -0500

    associative array

---
+++ assc_array.php
@@ -0,0 +1,26 @@
+<?php
+
+$tasks = [
+
+'title' => 'task-1',
+'descri' => 'Go to mall',
+'assigned_it' => 'deepu',
+'due' => 'today',
+'is_completed' => true
+];
+
+
+
+echo "<pre>";
+var_dump($tasks);
+echo "</pre>";
+
+echo '</br>';
+
+var_dump($tasks);
+
+echo '</br>';
+
+echo $tasks['is_completed'];
+
+?>
